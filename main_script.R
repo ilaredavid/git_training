63141a00e43b67fec034a8c53290683ca30c31d0
ilare <ilare@unizar.es>
2026-05-21 19:20:18 +0200

    Added necessary libraries to main script

--- main_script.R
+++ main_script.R
@@ -1,4 +1,23 @@
-library(ggplot2)
-library(dplyr)
+# Instalar multiples librerias (Si no lo estan)
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+packages<-c("data.table","purrr", "parallel", "mirai", "sf", "terra",
+            "arrow", "geoarrow", "duckdb")
+check.packages(packages)
+
+# Load libraries
+library(data.table)
+library(purr)
+library(parallel)
+library(mirai)
+library(sf)
+library(terra)
+library(arrow)
+library(geoarrow)
+library(duckdb)
 
 iris_data <- c(1,2,3,4)
